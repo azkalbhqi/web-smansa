ea6ca45a2ecd41bbbd4ea1a3d1ec7dd70efa311b
azkalbhqi <azkalpredator@gmail.com>
2021-11-11 09:48:33 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function navbar(){
+    const menuToggle = document.querySelector('.menu-toggle input');
+    const nav = document.querySelector('nav ul')
+
+    menuToggle.addEventListener('click' , nav.classList.toggle('slide'))
+}
+
