a4103aee24613a62bc11c26095624a1538130534
azkalbhqi <azkalpredator@gmail.com>
2021-11-22 08:28:54 +0700

    second commit

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function navbar(){
     menuToggle.addEventListener('click' , nav.classList.toggle('slide'))
 }
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycby8q2qdcFNZ8St3Z3Wh2Ip0tXVurByIkEdyrBohV-fWWvqL3IHQ0zqrq3Q7LuVmyMXSjA/exec'
+  const form = document.forms['aspirasi']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
+
